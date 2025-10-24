5c30464f4a476bf1b1d99317f71033e2989f0d76
Jose Arod Martinez Hernandez <160168999+Joseamh12345678@users.noreply.github.com>
2025-10-24 15:01:16 -0600

    API's completas

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
-from src.routes import users
+from backend.src.routes.rutas_route import router as rutas_router
+from backend.src.routes.geocode_route import router as geocode_router
+from backend.src.routes.directions_route import router as directions_router
+from backend.src.routes.matrix_route import router as matrix_router
+from backend.src.routes.optimize_route import router as optimize_router
+from backend.src.routes.users_route import router as users_router
 
-app = FastAPI(
-    title="Sistema de Optimizacion de Rutas",
-    description="API para gestion de usuarios y rutas",
-    version="1.0.0"
-)
 
-app.include_router(users.router)
+app = FastAPI()
+
+app.include_router(rutas_router)
+app.include_router(geocode_router)
+app.include_router(directions_router)
+app.include_router(matrix_router)
+app.include_router(optimize_router)
+app.include_router(users_router)
