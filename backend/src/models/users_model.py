ad18b28c1d339cc4554d3d8cd7f42fc233578564
Brandon Axel Alonso López <160175034+Brandon030605@users.noreply.github.com>
2025-10-29 14:07:43 -0700

    chequen eso

--- backend/src/models/users_model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
-
-class User(BaseModel):
-    id: Optional[int] = None
-    nombre: str
-    correo: EmailStr
-    edad: Optional[int] = None
--- backend/src/routes/geocode_route.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Query
-from backend.src.services.geocoding import geocode
-
-router = APIRouter(prefix="/geocode", tags=["Geocoding"])
-
-@router.get("/")
-def geocode_endpoint(address: str = Query(...)):
-    lat, lng = geocode(address)
-    return {"latitud": lat, "longitud": lng}
